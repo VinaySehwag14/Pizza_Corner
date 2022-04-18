56ac7fe0fcf8ea765e4bdd42a14efd3b85164b9a
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-04-18 15:10:26 +0530

    completed orders page

---
+++ pages/orders/[id].jsx
@@ -0,0 +1,96 @@
+import styles from "./order.module.css";
+import Image from "next/image";
+
+const Order = () => {
+  const status = 0;
+
+  const statusClass = (index) => {
+    if (index - status < 1) return styles.done;
+    if (index - status === 1) return styles.inProgress;
+    if (index - status > 1) return styles.undone;
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <div className={styles.row}>
+          <table className={styles.table}>
+            <tbody>
+              <tr className={styles.Title}>
+                <th>Order ID</th>
+                <th>Customer</th>
+                <th>Address</th>
+                <th>Total</th>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>
+                  <span className={styles.id}>1231454956</span>
+                </td>
+                <td>
+                  <span className={styles.name}>Vinay Sehwag</span>
+                </td>
+                <td>
+                  <span className={styles.address}>85A , Kanpur</span>
+                </td>
+                <td>
+                  <span className={styles.total}>$39.80</span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className={styles.row}>
+          <div className={statusClass(0)}>
+            <Image src="/image/paid.png" width={30} height={30} alt="" />
+            <span>Payment</span>
+            <div className={styles.checkedIcon}>
+              <Image src="/image/checked.png" width={20} height={20} alt="" />
+            </div>
+          </div>
+          <div className={statusClass(1)}>
+            <Image src="/image/bake.png" width={30} height={30} alt="" />
+            <span>Preparing</span>
+            <div className={styles.checkedIcon}>
+              <Image src="/image/checked.png" width={20} height={20} alt="" />
+            </div>
+          </div>
+          <div className={statusClass(2)}>
+            <Image src="/image/bike.png" width={30} height={30} alt="" />
+            <span>On the way</span>
+            <div className={styles.checkedIcon}>
+              <Image src="/image/checked.png" width={20} height={20} alt="" />
+            </div>
+          </div>
+          <div className={statusClass(3)}>
+            <Image src="/image/delivered.png" width={30} height={30} alt="" />
+            <span>Delivered</span>
+            <div className={styles.checkedIcon}>
+              <Image src="/image/checked.png" width={20} height={20} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.wrapper}>
+          <h2 className={styles.title}>CART TOTAL</h2>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Subtotal:</b>$79.60
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Discount:</b>$0.00
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Total:</b>$79.60
+          </div>
+          <button disabled className={styles.button}>
+            PAID
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Order;
