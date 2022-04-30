5db7ef9bd8e571f911bfef345362369e40c6ea24
VinaySehwag14 <vinaysehwag14@gmail.com>
2022-04-30 23:51:58 +0530

    admin page

---
+++ pages/admin/index.jsx
@@ -0,0 +1,10 @@
+import styles from "./Admin.module.css";
+
+const Admin = () => {
+  return;
+  <div className={styles.container}>
+    <div className={styles.item}></div>
+  </div>;
+};
+
+export default Admin;
